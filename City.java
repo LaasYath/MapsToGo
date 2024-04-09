6ca74acfabadd4b4d7c63794e0acb529a2169c4b
HKChiuy <144956998+HKChiuy@users.noreply.github.com>
2024-04-08 20:04:36 -0500

    Add files via upload

---
+++ City.java
@@ -0,0 +1,35 @@
+
+/**
+ * This is the class that represents the map/grid that will act as the city the user will traverse through.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+
+public class City
+{
+    // instance variables - replace the example below with your own
+    private Graph cityMap;
+
+    /**
+     * Constructor for objects of class CityMap
+     */
+    public CityMap()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
