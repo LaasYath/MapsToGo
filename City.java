212710991c3ee27da89e205cf47194f0354d13c0
HKChiuy <menfongchiu@gmail.com>
2024-04-08 20:14:44 -0500

    ahidopfa

--- City.java
+++ City.java
@@ -10,26 +10,14 @@ import java.util.*;
 public class City
 {
     // instance variables - replace the example below with your own
-    private Graph cityMap;
+    private WeightedAdjacencyListGraph city;
 
     /**
      * Constructor for objects of class CityMap
      */
-    public CityMap()
+    public City()
     {
         // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        city = null;
     }
 }
--- Dog.java
+++ Dog.java
@@ -1,6 +1,6 @@
 public class Dog {
 
-    public static void main(Stinr[] args){
+    public static void main(String[] args){
 
 
     }
